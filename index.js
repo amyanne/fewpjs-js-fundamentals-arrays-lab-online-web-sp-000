f0ed87101c3407a3f2d97df0d1cb879016b20f76
Amy Westcott <amy.westcott@yahoo.com>
2020-03-08 21:10:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie")
 prepend.unshift("Odie")
-removeLast.pop
-removeFirst.shift
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+
+append.push("Odie")
+prepend.unshift("Odie")
+removeLast.splice(-1)
+removeFirst.splice(0, 1)
